4c5dd27fa05803ad181ad6acc16ee310258aa556
Granit Gashi <33566885+GranitG@users.noreply.github.com>
2021-09-09 18:06:03 -0400

    Deletes posts by ID
    Check ifs user is authorized to remove the post (their own posts) and then verifies it and removes it.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -74,4 +74,33 @@ router.get('/:id', auth, async (req, res) => {
 	}
 });
 
+// @route       delete api/posts/:id
+// @desc        Deletes a post by the ID
+// @access      Private
+
+router.delete('/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// check if posts exist
+		if (!post) {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+		//Check if token = user so they can only delete their own posts
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'User not authorized' });
+		}
+
+		await post.remove();
+
+		res.json({ msg: 'Post Removed' });
+	} catch (err) {
+		console.error(err.message);
+		if (err.kind === 'ObjectId') {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+		res.status(500).send('Server Error');
+	}
+});
+
 module.exports = router;
