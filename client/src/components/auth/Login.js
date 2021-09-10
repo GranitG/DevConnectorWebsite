5c76dea1822af52c0136486d520c08e67c3963bc
Granit Gashi <33566885+GranitG@users.noreply.github.com>
2021-09-10 17:54:12 -0400

    Login and Register forms
    next step is to implement redux and allow checking to be done

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const Login = () => {
-	return <div>LogIn</div>;
+export const Login = () => {
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+	const { email, password } = formData;
+
+	const onChange = (e) =>
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		console.log('SUCCESS');
+	};
+
+	return (
+		<Fragment>
+			<h1 className='large text-primary'>Sign in</h1>
+			<p className='lead'>
+				<i className='fas fa-user' /> Sign into your account!
+			</p>
+			<form className='form' onSubmit={onSubmit}>
+				<div className='form-group'>
+					<input
+						type='email'
+						placeholder='Email Address'
+						name='email'
+						value={email}
+						onChange={onChange}
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Password'
+						name='password'
+						value={password}
+						onChange={onChange}
+					/>
+				</div>
+				<input type='submit' className='btn btn-primary' value='Login' />
+			</form>
+			<p className='my-1'>
+				Don't have an account? <Link to='/register'>Sign up</Link>
+			</p>
+		</Fragment>
+	);
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 export const Register = () => {
 	const [formData, setFormData] = useState({
@@ -14,12 +15,13 @@ export const Register = () => {
 
 	const onChange = (e) =>
 		setFormData({ ...formData, [e.target.name]: e.target.value });
+
 	const onSubmit = async (e) => {
 		e.preventDefault();
 		if (password !== password2) {
 			console.log('Passwords do not match');
 		} else {
-			console.log(formData);
+			console.log('SUCCESS');
 		}
 	};
 
